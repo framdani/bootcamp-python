f30399c8b1f958b63f857a04dbaa82c371f8b385
Fatima zahra Ramdani <framdani@e1r1p8.1337.ma>
2021-04-02 19:30:31 +0100

    ex07-remove punctuations

--- module00/ex07/filterwords.py
+++ module00/ex07/filterwords.py
@@ -7,9 +7,14 @@ else:
     if sys.argv[2].isdigit()==False or sys.argv[1].isdigit():
         print("ERROR")
     else:
-        new_words=sys.argv[1].split(' ')
+        punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        new_words=sys.argv[1]
+        for w in new_words:
+            for c in punc:
+                new_words=new_words.replace(c, "")
+        new_words=new_words.split(' ')
         return_value=[]
         for i in new_words:
             if len(i)>int(sys.argv[2]):
-                return_value.append(i)
+               return_value.append(i)
         print(return_value)
